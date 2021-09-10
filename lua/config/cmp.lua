9bb5073e574d4a429b08062c96526b34ccdac0d8
Kyle Wigley <kyle@fishtownanalytics.com>
2021-09-09 22:45:29 -0400

    turn off snippets, they are annoying rn

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -1,65 +1,66 @@
 local cmp = require("cmp")
-local luasnip = require("luasnip")
+-- local luasnip = require("luasnip")
 
-local check_back_space = function()
-  local col = vim.fn.col(".") - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
-end
+-- local check_back_space = function()
+--   local col = vim.fn.col(".") - 1
+--   return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+-- end
 
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
+-- local t = function(str)
+--   return vim.api.nvim_replace_termcodes(str, true, true, true)
+-- end
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
   completion = {
     completeopt = "menu,menuone,noinsert",
   },
+  -- snippet = {
+  --   expand = function(args)
+  --     luasnip.lsp_expand(args.body)
+  --   end,
+  -- },
   mapping = {
-    -- ["<CR>"] = cmp.mapping.confirm(), -- nvim-autopairs overrides this
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(t("<C-n>"), "n")
-      elseif luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
-      elseif check_back_space() then
-        vim.fn.feedkeys(t("<Tab>"), "n")
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(t("<C-p>"), "n")
-      elseif luasnip.jumpable(-1) then
-        vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
+    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+    -- ["<Tab>"] = cmp.mapping(function(fallback)
+    --   if vim.fn.pumvisible() == 1 then
+    --     vim.fn.feedkeys(t("<C-n>"), "n")
+    --   elseif luasnip.expand_or_jumpable() then
+    --     vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
+    --   elseif check_back_space() then
+    --     vim.fn.feedkeys(t("<Tab>"), "n")
+    --   else
+    --     fallback()
+    --   end
+    -- end, {
+    --   "i",
+    --   "s",
+    -- }),
+    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --   if vim.fn.pumvisible() == 1 then
+    --     vim.fn.feedkeys(t("<C-p>"), "n")
+    --   elseif luasnip.jumpable(-1) then
+    --     vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
+    --   else
+    --     fallback()
+    --   end
+    -- end, {
+    --   "i",
+    --   "s",
+    -- }),
   },
   sources = {
     { name = "nvim_lsp" },
-    { name = "buffer" },
-    { name = "luasnip" },
-    { name = "path" },
     { name = "nvim_lua" },
+    { name = "buffer" },
+    { name = "spell" },
+    { name = "path" },
+    { name = "crates" },
     { name = "calc" },
     { name = "emoji" },
-    { name = "spell" },
-    { name = "crates" },
+    -- { name = "luasnip" },
   },
 })
 
