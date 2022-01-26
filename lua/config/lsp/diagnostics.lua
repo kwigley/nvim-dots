a575c2784db2c92264105c6521d6cd18312e09e9
Kyle Wigley <kwigley44@gmail.com>
2022-01-25 19:35:45 -0500

    fix(vim): fix gutter diagnostic symbols

--- lua/config/lsp/diagnostics.lua
+++ lua/config/lsp/diagnostics.lua
@@ -11,12 +11,12 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 local signs = {
   Error = " ",
-  Warning = " ",
+  Warn = " ",
   Hint = " ",
-  Information = " ",
+  Info = " ",
 }
 
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
+  local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
