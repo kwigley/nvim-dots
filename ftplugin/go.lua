490cebffbc1ccb2191185faa3bd383b6be5870f6
Kyle Wigley <kwigley44@gmail.com>
2022-04-18 14:31:48 -0400

    only set tab for go buffers

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -1,6 +1,6 @@
 local indent = 4
-vim.opt.shiftwidth = indent
-vim.opt.tabstop = indent
-vim.opt.expandtab = false
+vim.opt_local.shiftwidth = indent
+vim.opt_local.tabstop = indent
+vim.opt_local.expandtab = false
 
 vim.bo.textwidth = 79 -- this is used for wrapping comments
