7e26ac60ff1e051cb7825a713ce926bc857977c4
Kyle Wigley <kwigley44@gmail.com>
2022-07-01 10:45:13 -0400

    add nvim-surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -149,6 +149,48 @@ local function plugins(use)
     },
   })
 
+  -- surround selections
+  use({
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup({
+        keymaps = { -- vim-surround style keymaps
+          insert = "ys",
+          visual = "S",
+          delete = "ds",
+          change = "cs",
+        },
+        delimiters = {
+          pairs = {
+            ["("] = { "( ", " )" },
+            [")"] = { "(", ")" },
+            ["{"] = { "{ ", " }" },
+            ["}"] = { "{", "}" },
+            ["<"] = { "< ", " >" },
+            [">"] = { "<", ">" },
+            ["["] = { "[ ", " ]" },
+            ["]"] = { "[", "]" },
+          },
+          separators = {
+            ["'"] = { "'", "'" },
+            ['"'] = { '"', '"' },
+            ["`"] = { "`", "`" },
+          },
+          HTML = {
+            ["t"] = true, -- Use "t" for HTML-style mappings
+          },
+          aliases = {
+            ["a"] = ">", -- Single character aliases apply everywhere
+            ["b"] = ")",
+            ["B"] = "}",
+            ["r"] = "]",
+            ["q"] = { '"', "'", "`" }, -- Table aliases only apply for changes/deletions
+          },
+        },
+      })
+    end,
+  })
+
   -- Treesitter: syntax highlighting
   use({
     "nvim-treesitter/nvim-treesitter",
