7cc42d24e8c27680fbb28d14d3cd15950f795ee1
Kyle Wigley <mail@kylewigs.com>
2022-07-19 11:52:10 -0400

    fix formatting

--- lua/config/lsp/diagnostics.lua
+++ lua/config/lsp/diagnostics.lua
@@ -1,13 +1,11 @@
 -- Automatically update diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics,
-  {
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
     update_in_insert = false,
     virtual_text = { spacing = 4, prefix = "●" },
     severity_sort = true,
-  }
-)
+  })
 
 local signs = {
 
