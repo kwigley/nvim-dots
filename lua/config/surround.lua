866054bb055d5d65d8a0fd1dae2a0a7227caaea6
Kyle Wigley <mail@kylewigs.com>
2022-08-08 10:24:27 -0400

    use default config for nvim-surround

--- lua/config/surround.lua
+++ lua/config/surround.lua
@@ -1,41 +1,7 @@
 local M = {}
 
 function M.setup()
-  require("nvim-surround").setup({
-    keymaps = { -- vim-surround style keymaps
-      insert = "ys",
-      visual = "S",
-      delete = "ds",
-      change = "cs",
-    },
-    delimiters = {
-      pairs = {
-        ["("] = { "( ", " )" },
-        [")"] = { "(", ")" },
-        ["{"] = { "{ ", " }" },
-        ["}"] = { "{", "}" },
-        ["<"] = { "< ", " >" },
-        [">"] = { "<", ">" },
-        ["["] = { "[ ", " ]" },
-        ["]"] = { "[", "]" },
-      },
-      separators = {
-        ["'"] = { "'", "'" },
-        ['"'] = { '"', '"' },
-        ["`"] = { "`", "`" },
-      },
-      HTML = {
-        ["t"] = true, -- Use "t" for HTML-style mappings
-      },
-      aliases = {
-        ["a"] = ">", -- Single character aliases apply everywhere
-        ["b"] = ")",
-        ["B"] = "}",
-        ["r"] = "]",
-        ["q"] = { '"', "'", "`" }, -- Table aliases only apply for changes/deletions
-      },
-    },
-  })
+  require("nvim-surround").setup({})
 end
 
 return M
