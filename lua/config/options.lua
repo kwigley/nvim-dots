644630bf761000906f68caa7c7648022de12664a
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-03-13 12:40:42 -0400

    rm ufo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,19 +5,6 @@
 vim.opt.shortmess:append("I") -- don't give the intro message when starting Vim |:intro|
 vim.opt.relativenumber = false -- Relative line numbers
 
-vim.opt.fillchars = {
-  foldopen = "",
-  foldclose = "",
-  fold = " ",
-  foldsep = " ",
-  diff = "╱",
-  eob = " ",
-}
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-vim.o.foldcolumn = "0"
-
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -183,21 +183,4 @@ return {
       },
     },
   },
-
-  -- add nvim-ufo
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    event = "BufReadPost",
-    opts = {},
-
-    init = function()
-      vim.keymap.set("n", "zR", function()
-        require("ufo").openAllFolds()
-      end)
-      vim.keymap.set("n", "zM", function()
-        require("ufo").closeAllFolds()
-      end)
-    end,
-  },
 }
