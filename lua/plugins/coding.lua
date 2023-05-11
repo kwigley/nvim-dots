7430cd4c515323f0e2d338dabe872222f213cbbe
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-05-11 14:42:50 -0400

    copilot stinks

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      filetypes = {
-        yaml = true,
-        markdown = true,
-        gitcommit = true,
-      },
-    },
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   opts = {
+  --     filetypes = {
+  --       yaml = true,
+  --       markdown = true,
+  --       gitcommit = true,
+  --     },
+  --   },
+  -- },
   {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
