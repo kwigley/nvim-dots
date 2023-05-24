2986016492a82986c3c8dfdd15f8252070482323
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-05-24 09:01:26 -0400

    add more extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -44,6 +44,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
   },
