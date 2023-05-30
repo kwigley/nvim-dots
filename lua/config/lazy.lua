2ff90ea2b21ef2c3a5ceacf5e695ba7753009882
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-05-30 13:33:49 -0400

    dep updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -39,6 +39,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
