6144ac6b3da6370ee7c4053eead7b846e828ea08
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-06-15 09:08:20 -0400

    rm animation

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      show_current_context = true,
+    },
+  },
+  { "echasnovski/mini.indentscope", enabled = false },
+  {
     "rcarriga/nvim-notify",
     opts = { top_down = false },
   },
