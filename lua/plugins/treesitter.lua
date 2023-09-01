fd76f455d64166999aecf789edc0f97ccea5b9d7
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-09-01 08:51:49 -0400

    turn off TS context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      -- nvim-ts-autotag
       autotag = {
         enable = true,
       },
@@ -19,6 +18,7 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
+    enabled = false,
     event = "BufReadPre",
     config = true,
     opts = { mode = "cursor" },
