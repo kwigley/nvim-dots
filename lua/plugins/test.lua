2ac61855b340c1de4193b082b893b126728adddb
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-10-31 10:17:49 -0400

    turn off autoformat

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -2,6 +2,14 @@ return {
   { "nvim-neotest/neotest-plenary" },
   {
     "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-plenary" } },
+    opts = {
+      adapters = {
+        ["neotest-plenary"] = {},
+        ["neotest-python"] = {
+          runner = "pytest",
+          python = ".venv/bin/python",
+        },
+      },
+    },
   },
 }
