1acf46f7980abcc29063ce8dfb4821f935c55747
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2024-04-04 22:33:51 -0400

    update deps

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -18,4 +18,52 @@ return {
     },
     opts = { snippet_engine = "luasnip" },
   },
+  {
+    "kawre/neotab.nvim",
+    event = "InsertEnter",
+    opts = {
+      tabkey = "",
+    },
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    event = "VeryLazy",
+    keys = {
+      { "<tab>", mode = { "i", "s" }, false },
+      { "<s-tab>", mode = { "i", "s" }, false },
+      {
+        "<Tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next"
+            or "<Plug>(neotab-out)"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      {
+        "<Tab>",
+        function()
+          require("luasnip").jump(1)
+        end,
+        mode = "s",
+      },
+      {
+        "<S-Tab>",
+        function()
+          return require("luasnip").jump(-1) or "<Plug>(TaboutBack)"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      {
+        "<S-Tab>",
+        function()
+          require("luasnip").jump(-1)
+        end,
+        mode = "s",
+      },
+    },
+  },
 }
