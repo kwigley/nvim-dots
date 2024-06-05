2a3a754f5161c1c24d7d584b0999bd71695deebc
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2024-06-05 13:36:10 -0400

    fix crash

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,32 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
--- ftdetect
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = "*.fish", command = "setfiletype fish" }
-)
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = "*.nix", command = "setfiletype nix" }
-)
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = "*.hcl", command = "setfiletype hcl" }
-)
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = ".terraform,terraform.rc", command = "setfiletype hcl" }
-)
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = "*.tf,*.tfvars", command = "setfiletype terraform" }
-)
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = "*.tfstate,*.tfvars.backup", command = "setfiletype json" }
-)
-
 -- show cursor line only in active window
 vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
   callback = function()
@@ -51,7 +25,7 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
   callback = function(event)
-    local file = vim.loop.fs_realpath(event.match) or event.match
+    local file = vim.uv.fs_realpath(event.match) or event.match
 
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
     local backup = vim.fn.fnamemodify(file, ":p:~:h")
@@ -74,19 +48,4 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     vim.bo.textwidth = 80
   end,
-})
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  desc = "Auto select virtualenv Nvim open",
-  pattern = "*",
-  callback = function()
-    local ok, venv_selector = pcall(require, "venv-selector")
-    if ok then
-      local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-      if venv ~= "" then
-        venv_selector.retrieve_from_cache()
-      end
-    end
-  end,
-  once = true,
 })
