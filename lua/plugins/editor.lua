438e580ed8bb970af0bb80b086d20bdf4236df2d
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2025-02-19 20:22:24 -0500

    rm venv

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,26 +1,5 @@
 return {
   {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "mfussenegger/nvim-dap",
-      "mfussenegger/nvim-dap-python", --optional
-      {
-        "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
-        dependencies = { "nvim-lua/plenary.nvim" },
-      },
-    },
-    lazy = false,
-    branch = "regexp",
-    config = function()
-      require("venv-selector").setup()
-    end,
-    keys = {
-      { ",v", "<cmd>VenvSelect<cr>" },
-    },
-  },
-  {
     "Wansmer/treesj",
     keys = { { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" } },
     opts = { use_default_keymaps = false, max_join_length = 150 },
