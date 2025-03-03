f3020f2aec4ca638864ebd24126117e8d7a2dd0f
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2025-03-03 07:49:31 -0500

    asdf

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      model = "claude-3.7-sonnet",
+    },
+  },
+  {
     "Wansmer/treesj",
     keys = { { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" } },
     opts = { use_default_keymaps = false, max_join_length = 150 },
