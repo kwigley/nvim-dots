f1731ecea5de28d4d50308402e1ee2ea7d3a8858
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2025-04-01 11:05:25 -0400

    add commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,14 +6,3 @@
 vim.keymap.set("n", ":Q", ":q<CR>")
 vim.keymap.set("n", ":W", ":w<CR>")
 vim.keymap.set("n", "<C-s>", ":w<CR>")
-
-vim.api.nvim_create_user_command("CopyPath", function()
-  local full_path = vim.fn.glob("%:p")
-  vim.fn.setreg("+", full_path)
-  vim.print("Filepath copied to clipboard!")
-end, {
-  bang = false,
-  nargs = 1,
-  force = true,
-  desc = "Copy current file path to clipboard",
-})
