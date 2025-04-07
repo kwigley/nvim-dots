47c8af6035b2a5e0c4f6a32c26542057bc584d6a
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2025-04-07 16:42:04 -0400

    update deps

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -27,3 +27,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.bo.textwidth = 80
   end,
 })
+
+vim.api.nvim_create_user_command("CopyPath", function()
+  local full_path = vim.fn.glob("%:p")
+  vim.fn.setreg("+", full_path)
+  vim.print("Filepath copied to clipboard!")
+end, {
+  bang = false,
+  nargs = 0,
+  force = true,
+  desc = "Copy current file path to clipboard",
+})
