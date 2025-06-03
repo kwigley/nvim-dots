501ffc8c64695a401348ff0c8671d955d43aa42d
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2025-06-02 22:01:00 -0400

    add lsp settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,53 @@ return {
     opts = {
       diagnostics = { virtual_text = { prefix = "icons" } },
       servers = {
+        eslint = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+            "vue",
+            "html",
+            "markdown",
+            "json",
+            "jsonc",
+            "yaml",
+            "toml",
+            "xml",
+            "gql",
+            "graphql",
+            "astro",
+            "svelte",
+            "css",
+            "less",
+            "scss",
+            "pcss",
+            "postcss",
+          },
+          settings = {
+            rulesCustomizations = {
+              { rule = "style/*", severity = "off", fixable = true },
+              { rule = "format/*", severity = "off", fixable = true },
+              { rule = "*-indent", severity = "off", fixable = true },
+              { rule = "*-spacing", severity = "off", fixable = true },
+              { rule = "*-spaces", severity = "off", fixable = true },
+              { rule = "*-order", severity = "off", fixable = true },
+              { rule = "*-dangle", severity = "off", fixable = true },
+              { rule = "*-newline", severity = "off", fixable = true },
+              { rule = "*quotes", severity = "off", fixable = true },
+              { rule = "*semi", severity = "off", fixable = true },
+            },
+          },
+          on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              command = "EslintFixAll",
+            })
+          end,
+        },
         helm_ls = {
           ["helm-ls"] = {
             yamlls = {
