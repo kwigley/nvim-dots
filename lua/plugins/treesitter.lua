a2916c64ac74f7e2a473b12123a64379309e18e0
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2025-09-22 16:58:48 -0400

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,12 +7,6 @@ return {
       },
       incremental_selection = {
         enable = true,
-        keymaps = {
-          init_selection = "<C-n>",
-          node_incremental = "<C-n>",
-          scope_incremental = "<C-s>",
-          node_decremental = "<C-r>",
-        },
       },
     },
   },
