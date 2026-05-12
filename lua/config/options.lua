e8526d73d8cff1c3bf2a2f7750b1aeb16358e1ca
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2026-05-12 15:34:18 -0400

    updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,3 +16,9 @@ vim.g.autoformat = false
 
 -- Native inline completions don't support being shown as regular completions
 vim.g.ai_cmp = false
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+-- vim.g.lazyvim_python_lsp = "basedpyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
