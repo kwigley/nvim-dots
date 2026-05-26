500a8b9d723d6dd76a4462bc15a18b28af3faa60
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2026-05-26 11:52:55 -0400

    use codeium

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,3 +19,5 @@ vim.g.autoformat = false
 -- vim.g.lazyvim_python_lsp = "basedpyright"
 -- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
+
+vim.g.ai_cmp = false
