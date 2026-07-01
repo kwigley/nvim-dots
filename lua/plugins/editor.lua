3d068660b28e12cf799bd07d052143db24f02be2
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2026-07-01 16:23:43 -0400

    updates

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -23,4 +23,47 @@ return {
     keys = { { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" } },
     opts = { use_default_keymaps = false, max_join_length = 150 },
   },
+  {
+    "wasabeef/yank-for-claude.nvim",
+    config = function()
+      require("yank-for-claude").setup()
+    end,
+    keys = {
+      -- Reference only
+      {
+        "<leader>cy",
+        function()
+          require("yank-for-claude").yank_visual()
+        end,
+        mode = "v",
+        desc = "Yank for Claude",
+      },
+      {
+        "<leader>cy",
+        function()
+          require("yank-for-claude").yank_line()
+        end,
+        mode = "n",
+        desc = "Yank line for Claude",
+      },
+
+      -- Reference + Code
+      {
+        "<leader>cY",
+        function()
+          require("yank-for-claude").yank_visual_with_content()
+        end,
+        mode = "v",
+        desc = "Yank with content",
+      },
+      {
+        "<leader>cY",
+        function()
+          require("yank-for-claude").yank_line_with_content()
+        end,
+        mode = "n",
+        desc = "Yank line with content",
+      },
+    },
+  },
 }
